c80366af6a8f8dbf4dcc12b9eb43841b45f262aa
ouikene koceila <koceila.ouikene@worldline.com>
2019-03-03 17:04:59 +0100

    Finish part q6

---
+++ src/main/java/com/miage/altea/tp/pokemon_ui/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.miage.altea.tp.pokemon_ui.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@Controller
+@SpringBootApplication
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @PostMapping("/registerTrainer")
+    ModelAndView registerNewTrainer(@RequestParam String trainerName){
+        ModelAndView modelView = new ModelAndView("register");
+        modelView.getModel().put("name", trainerName);
+        return modelView;
+    }
+}
