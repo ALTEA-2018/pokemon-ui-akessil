4617074000890f46cc1cc30994b4a866a99c3c43
ouikene koceila <koceila.ouikene@worldline.com>
2019-04-05 16:38:45 +0200

    add Profile Controller

--- src/main/java/com/miage/altea/tp/pokemon_ui/controller/ProfileController.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/controller/ProfileController.java
@@ -5,16 +5,14 @@ import com.miage.altea.tp.pokemon_ui.bo.TrainerUI;
 import com.miage.altea.tp.pokemon_ui.mapper.PokemonMapper;
 import com.miage.altea.tp.pokemon_ui.mapper.TrainerMapper;
 import com.miage.altea.tp.pokemon_ui.pokemonsType.bo.PokemonType;
+import com.miage.altea.tp.pokemon_ui.pokemonsType.service.PokemonTypeService;
 import com.miage.altea.tp.pokemon_ui.trainers.bo.Pokemon;
 import com.miage.altea.tp.pokemon_ui.trainers.bo.Trainer;
-import com.miage.altea.tp.pokemon_ui.pokemonsType.service.PokemonTypeService;
 import com.miage.altea.tp.pokemon_ui.trainers.service.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
 
 import java.security.Principal;
 import java.util.ArrayList;
@@ -22,20 +20,20 @@ import java.util.List;
 
 @Controller
 @SpringBootApplication
-public class TrainerController {
+public class ProfileController {
+
     private TrainerService trainerService;
     private PokemonTypeService pokemonTypeService;
 
     private TrainerMapper traionerMapper = TrainerMapper.getInstance();
     private PokemonMapper pokemonMapper = PokemonMapper.getInstance();
 
-    @GetMapping("/trainers")
-    public ModelAndView trainers(){
-        List<Trainer> trainers = trainerService.listAllTrainers();
-        List<TrainerUI> trainersUI = getTrainersUI(trainers);
-        ModelAndView modelAndView = new ModelAndView("trainers");
-        modelAndView.getModel().put("trainers", trainersUI);
-        return modelAndView;
+    @GetMapping("/profile")
+    public TrainerUI getProfile(Principal principal){
+        if(principal == null) {
+            return null;
+        }
+        return getTrainerUI(trainerService.getTrainer(principal.getName()));
     }
 
     @Autowired
@@ -47,6 +45,7 @@ public class TrainerController {
     public void setPokemonTypeService(PokemonTypeService pokemonTypeService) {
         this.pokemonTypeService = pokemonTypeService;
     }
+
 
     private List<TrainerUI> getTrainersUI(List<Trainer> trainers){
         List<TrainerUI> result = new ArrayList<>();
@@ -76,6 +75,5 @@ public class TrainerController {
         }
         return pokemonsUI;
     }
-
 
 }
