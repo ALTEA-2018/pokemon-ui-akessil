4d00faa34424eab12e7587a1d3bcccfbf6196b3f
ouikene koceila <koceila.ouikene@worldline.com>
2019-04-06 16:24:37 +0200

    Fix small bug

--- src/main/java/com/miage/altea/tp/pokemon_ui/controller/BattleController.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/controller/BattleController.java
@@ -1,16 +1,21 @@
 package com.miage.altea.tp.pokemon_ui.controller;
 
 import org.springframework.boot.actuate.trace.http.HttpTrace;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
+
 @Controller
+@SpringBootApplication
 public class BattleController {
 
     @GetMapping("/fight/{opponent}")
-    public ModelAndView fight(HttpTrace.Principal principal, @PathVariable String opponent){
+    public ModelAndView fight(Principal principal, @PathVariable String opponent){
+        System.out.println("--------------BattleController trainer: " + principal.getName() + " -> fight -> opponent:" + opponent);
         var modelAndView = new ModelAndView("fight");
 
         modelAndView.addObject("trainerName", principal.getName());
